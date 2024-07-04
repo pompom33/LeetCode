576273f8da9d8b04bb2375758b070dc19601fcf6
YUNA AN <nymphyuna@gmail.com>
2024-07-04 09:36:19 +0900

    Time: 731 ms (77.66%) | Memory: 16.9 MB (84.83%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.py
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.py
@@ -1,13 +1,8 @@
-# 1. mat를 순회하며 1의 개수를 센다
-# 2. 1의 개수가 가장 많은 원소를 구한다 = am
-# 3. 그 원소를 반환한다
-
 class Solution:
     def rowAndMaximumOnes(self, mat: List[List[int]]) -> List[int]:
-        tmp = []
-        for i in range(len(mat)):
-            count_one = mat[i].count(1)
-            tmp.append(count_one)
-        for j in range(len(tmp)):
-            if max(tmp) == tmp[j]:
-                return [j, max(tmp)]
+        data = []
+        for i, row in enumerate(mat):
+            count_one = row.count(1)
+            data.append([i, count_one])
+        answer = sorted(data, key =lambda x:x[1], reverse=True)
+        return answer[0]
