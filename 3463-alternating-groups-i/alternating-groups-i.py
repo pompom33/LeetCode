2a4098eefdf2192ebfea7e69dd500586328edaee
YUNA AN <nymphyuna@gmail.com>
2024-07-17 09:37:08 +0900

    Time: 58 ms (18.14%) | Memory: 16.5 MB (81.89%) - LeetSync

--- 3463-alternating-groups-i/alternating-groups-i.py
+++ 3463-alternating-groups-i/alternating-groups-i.py
@@ -1,7 +1,8 @@
 class Solution:
     def numberOfAlternatingGroups(self, colors: List[int]) -> int:
         answer = 0
+        length = len(colors)
         for i in range(len(colors)):
-            if colors[i-2] != colors[i-1] and colors[i-1] != colors[i]:
+            if colors[i-1] != colors[i] and colors[(i+1) % length] != colors[i]:
                 answer += 1
         return answer
