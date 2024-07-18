4f98e6f07a210a6bd9bd17f95dc3105f753468f6
YUNA AN <nymphyuna@gmail.com>
2024-07-18 15:11:36 +0900

    Time: 32 ms (80.34%) | Memory: 16.5 MB (64.41%) - LeetSync

--- 2088-minimum-time-to-type-word-using-special-typewriter/minimum-time-to-type-word-using-special-typewriter.py
+++ 2088-minimum-time-to-type-word-using-special-typewriter/minimum-time-to-type-word-using-special-typewriter.py
@@ -6,7 +6,6 @@ class Solution:
             new_loc = ord(c)
             clockwise = abs(new_loc - cur_loc)
             counter_clockwise = 26 - abs(new_loc - cur_loc)
-            print(cur_loc, min(clockwise, counter_clockwise), clockwise, counter_clockwise)
             answer += min(clockwise, counter_clockwise)
             cur_loc = new_loc
         return answer + len(word)
