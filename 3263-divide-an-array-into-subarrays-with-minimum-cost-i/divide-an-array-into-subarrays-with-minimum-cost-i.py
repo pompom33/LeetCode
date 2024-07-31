407ff2ba596b805e6df50e8fba7b7b6cf68b9f1c
YUNA AN <nymphyuna@gmail.com>
2024-07-31 10:09:10 +0900

    Time: 88 ms (8.99%) | Memory: 16.5 MB (22.81%) - LeetSync

--- 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.py
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.py
@@ -1,8 +1,8 @@
 class Solution:
-    def minimumCost(self, nums: List[int]) -> int: 
-        answer = []
+    def minimumCost(self, nums: List[int]) -> int:
+        answer = float('inf')
         for i in range(1, len(nums)):
             for j in range(i+1, len(nums)):
-                sum = nums[0] + nums[i] + nums[j]
-                answer.append(sum)
-        return min(answer)
+                sum_ = nums[0] + nums[i] + nums[j]
+                answer = min(sum_, answer)
+        return answer
