0d2fce9f87ace5d1b49df5365605e4490343914d
YUNA AN <nymphyuna@gmail.com>
2024-08-01 10:11:19 +0900

    Time: 47 ms (47.41%) | Memory: 16.6 MB (64.12%) - LeetSync

--- 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.py
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.py
@@ -1,7 +1,3 @@
 class Solution:
     def mostWordsFound(self, sentences: List[str]) -> int:
-        answer = float("-inf")
-        for sentence in sentences:
-            answer = max(answer, len(sentence.split()))
-        return answer
-            
+        return max([len(x.split()) for x in sentences])
