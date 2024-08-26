0e3773ed05d26335cc5f3ef2c0f9a9bae34a71a2
YUNA AN <nymphyuna@gmail.com>
2024-08-26 09:43:26 +0900

    Time: 107 ms (67.96%) | Memory: 17.3 MB (84.89%) - LeetSync

--- 2447-merge-similar-items/merge-similar-items.py
+++ 2447-merge-similar-items/merge-similar-items.py
@@ -1,12 +1,7 @@
 class Solution:
     def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
         dict = {}
-        answer = []
-        for item in items1:
+        for item in items1 + items2:
             dict[item[0]] = dict.get(item[0], 0) + item[1]
-        for item in items2:
-            dict[item[0]] = dict.get(item[0], 0) + item[1]
-        for key, value in dict.items():
-            answer.append([key, value])
-        answer = sorted(answer)
-        return answer
+        answer = [[key, value] for key, value in dict.items()]    
+        return sorted(answer)
