8a5205d19cebc1b74aae62fb1f4a46269ee0347a
YUNA AN <nymphyuna@gmail.com>
2024-08-29 14:24:08 +0900

    Time: 39 ms (28.00%) | Memory: 16.5 MB (73.85%) - LeetSync

--- 1812-reformat-phone-number/reformat-phone-number.py
+++ 1812-reformat-phone-number/reformat-phone-number.py
@@ -1,18 +1,16 @@
 class Solution:
     def reformatNumber(self, number: str) -> str:
-        tmp = ""
-        answer = ""
-        for c in number:
-            if c.isdigit():
-                tmp += c     
-        for i in range(0, len(tmp), 3):
-            if len(tmp) % 3 == 1:
-                if i == len(tmp)-4:
-                    answer = answer + tmp[i:i+2] + "-"
-                    answer = answer + tmp[i+2:]
-                    break
-            if i >= len(tmp)-3:
-                answer = answer + tmp[i:i+3]
-                break
-            answer = answer + tmp[i:i+3] + "-"
-        return answer
+        number = number.replace(' ', "")
+        number = number.replace('-', "")
+        answer = []
+        while len(number) > 4:
+            answer.append(number[:3])
+            number = number[3:]
+        if len(number) == 4:
+            answer.append(number[:2])
+            number = number[2:]
+            answer.append(number[:2])
+            number = number[2:]
+        else:
+            answer.append(number[:3])
+        return "-".join(answer)
