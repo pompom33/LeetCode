88fd8b7c9f38ca990b7414773ca303a7193d89a0
YUNA AN <nymphyuna@gmail.com>
2024-08-30 10:06:33 +0900

    Time: 51 ms (27.06%) | Memory: 16.5 MB (72.36%) - LeetSync

--- 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.py
+++ 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.py
@@ -1,8 +1,3 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        cnt = 0
-        for num in nums:
-            if num < k:
-                del num
-                cnt += 1
-        return cnt
+        return len([num for num in nums if num < k])
