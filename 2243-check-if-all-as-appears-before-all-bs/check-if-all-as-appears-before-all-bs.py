747bcc72b89883cc7d37be2a6d75a7dd11437825
YUNA AN <nymphyuna@gmail.com>
2024-09-19 09:29:07 +0900

    Time: 27 ms (94.43%) | Memory: 16.5 MB (13.07%) - LeetSync

--- 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.py
+++ 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.py
@@ -1,6 +1,3 @@
 class Solution:
     def checkString(self, s: str) -> bool:
-        answer = True
-        if "ba" in s:
-            answer = False
-        return answer
+        return "ba" not in s
