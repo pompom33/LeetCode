b8efe525d7564d207020199b1f1bea0fd1a10e32
YUNA AN <nymphyuna@gmail.com>
2024-10-06 15:26:46 +0900

    Time: 226 ms (42.86%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,7 +1,8 @@
-# Write your MySQL query statement below
-SELECT S.machine_id, ROUND(AVG(E.timestamp-S.timestamp), 3) AS processing_time
-FROM Activity S
-JOIN Activity E
-    ON S.machine_id = E.machine_id
-WHERE S.process_id = E.process_id AND S.activity_type='start' AND E.activity_type='end'
-GROUP BY S.machine_id;
+SELECT machine_id, 
+        ROUND(
+            AVG(
+                CASE WHEN activity_type='start' THEN - timestamp ELSE timestamp end
+                )*2,
+                3) AS processing_time
+FROM activity
+GROUP BY machine_id;
