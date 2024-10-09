bd33070e6a5d457c1d780ba719a4c3cd7cb9143e
YUNA AN <nymphyuna@gmail.com>
2024-10-09 16:33:13 +0900

    Time: 1105 ms (14.39%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -9,4 +9,4 @@ LEFT JOIN Examinations E
     ON S.student_id = E.student_id
     AND sub.subject_name = E.subject_name
 GROUP BY S.student_id, S.student_name, sub.subject_name
-ORDER BY S.student_id, S.student_name, sub.subject_name
+ORDER BY S.student_id, sub.subject_name
