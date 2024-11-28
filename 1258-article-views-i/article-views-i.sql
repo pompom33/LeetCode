0c5ea0399d6e8e7d729f1ea98661ca5d01990390
YUNA AN <nymphyuna@gmail.com>
2024-11-28 10:27:12 +0900

    Time: 518 ms (63.63%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,5 +1,5 @@
-# Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
 SELECT DISTINCT author_id AS id
 FROM Views
-WHERE viewer_id = author_id
-ORDER BY id;
+WHERE author_id = viewer_id
+ORDER BY author_id;
