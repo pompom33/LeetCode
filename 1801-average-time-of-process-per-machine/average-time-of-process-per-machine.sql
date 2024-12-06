66a88a85a404fa3c468d7751236696fe50f29b69
YUNA AN <nymphyuna@gmail.com>
2024-12-06 11:27:39 +0900

    Time: 405 ms (56.54%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,8 +1,8 @@
-SELECT machine_id, 
-        ROUND(
-            AVG(
-                CASE WHEN activity_type='start' THEN - timestamp ELSE timestamp end
-                )*2,
-                3) AS processing_time
-FROM activity
-GROUP BY machine_id;
+/* Write your PL/SQL query statement below */
+SELECT E.machine_id, 
+        ROUND(AVG(E.timestamp - S.timestamp), 3) AS processing_time
+FROM Activity E
+JOIN Activity S
+    ON E.machine_id = S.machine_id 
+WHERE S.activity_type = 'start' AND E.activity_type = 'end'
+GROUP BY E.machine_id;
